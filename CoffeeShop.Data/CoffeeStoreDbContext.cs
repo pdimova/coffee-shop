999b7c88cc764c7e624e895c81368304b73d7d00
pollx <aurinko@abv.bg>
2017-09-27 18:45:57 +0300

    Db context added

---
+++ CoffeeShop.Data/CoffeeStoreDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CoffeeShop.Data.Models;
+using System.Data.Entity;
+
+namespace CoffeeShop.Data
+{
+    public class CoffeeShopDbContext : DbContext
+    {
+        public CoffeeShopDbContext() : base("CoffeeShop")
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<CoffeeShopDbContext>());
+        }
+
+        public DbSet<Cart> Carts { get; set; }
+        //public DbSet<Order> Orders { get; set; }
+        //public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
